49c9b4f377ad85196794c16bb6ce6eb3615185c6
Ryan Golhar <ryan.golhar@bms.com>
2019-12-30 22:17:51 -0500

    Add BatchJob to flask shell

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ Main application entry point
 from os.path import dirname, join
 from dotenv import load_dotenv
 from app import create_app, DB
-from app.models import Notification, User
+from app.models import BatchJob, Notification, User
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -14,7 +14,8 @@ application = create_app()
 @application.shell_context_processor
 def make_shell_context():
     # For this to work, make sure FLASK_APP=application.py is set
-    return {'db': DB, 'Notification': Notification, 'User': User}
+    return {'db': DB, 'BatchJob': BatchJob, 'Notification': Notification,
+            'User': User}
 
 # Run the application
 if __name__ == "__main__":
