507797adb28841795ac46eddc34fd5a4cbfd1770
Ryan Golhar <ryan.golhar@bms.com>
2020-01-23 21:18:49 -0500

    Add /api/v0/test

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,8 +1,10 @@
 '''
 Base of REST API
 '''
-
-from flask import Blueprint
+import pkg_resources
+import os
+from datetime import timedelta
+from flask import Blueprint, current_app, request, session
 from flask_restplus import Api
 
 from .jobs import NS as jobs_ns
@@ -14,6 +16,50 @@ from .users import NS as users_ns
 from .xpress import NS as xpress_ns
 
 BLUEPRINT = Blueprint('rest_api', __name__, url_prefix='/api/v0')
+
+@BLUEPRINT.route("/test")
+def apiTest():
+    data = {
+        'Package Versions': {},
+        'Session Variables': {},
+        'Cookie Variables': {},
+        'Request Headers': {},
+        'Environment Variables': {},
+        'Application Settings': {}
+    }
+
+    # Package Versions
+    dists = [str(d).replace(" ",":") for d in pkg_resources.working_set]
+    for dist in dists:
+        pkgName, pkgVersion = dist.split(':')
+        data['Package Versions'][pkgName] = pkgVersion
+
+    # Session Variables
+    for s in session:
+        data['Session Variables'][s] = session[s]
+
+    # Cookie Variables
+    for c in request.cookies:
+        data['Cookie Variables'][c] = request.cookies.get(c)
+
+    # Request Headers
+    for h in request.headers.keys():
+        data['Request Headers'][h] = request.headers.get(h)
+
+    # Environment Variables
+    for k,v in sorted(os.environ.items()):
+        data['Environment Variables'][k] = v
+
+    # Application Settings
+    for k in current_app.config:
+        if isinstance(current_app.config[k], (timedelta)):
+            data['Application Settings'][k] = str(current_app.config[k])
+        else:
+            data['Application Settings'][k] = current_app.config[k]
+    #data['Application Settings']['debug'] = current_app.debug
+    #data['Application Settings']['testing'] = current_app.testing
+    
+    return data
 
 API = Api(BLUEPRINT,
           title="NGS360 REST API",
