6fd6bce02c1b9653e6502b05b700779bd7b49b0e
Ryan Golhar <ryan.golhar@bms.com>
2020-02-26 16:22:52 -0500

    Working on making browseFilesDialog work

--- app/static/browseFiles.js
+++ app/static/browseFiles.js
@@ -1,7 +1,11 @@
-browseFiles = function (bucket, prefix) {
+browseFilesDialog = function (bucket, prefix) {
     // bucket
     this.bucket = bucket;
     // prefix of directory in bucket
     this.prefix = prefix;
     console.log("Browsing s3://" + bucket + "/" + prefix);
+};
+
+browseFilesDialog.prototype.show = function () {
+    console.log("Show the modal dialog");
 };
