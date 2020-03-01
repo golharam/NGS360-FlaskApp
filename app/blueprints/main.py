74c69539231b122bacbd55334aaf0b400fadce22
Ryan Golhar <ryan.golhar@bms.com>
2020-02-29 22:15:10 -0500

    Fix job log page

--- app/blueprints/main.py
+++ app/blueprints/main.py
@@ -2,9 +2,9 @@
 Main application endpoints
 '''
 from flask import Blueprint, render_template, request, session, jsonify, current_app, abort
-from flask_login import login_required
+from flask_login import login_required, current_user
 from app import project_registry, BASESPACE as base_space, DB as db
-from app.models import Project, RunToSamples, SequencingRun
+from app.models import Project, RunToSamples, SequencingRun, BatchJob
 
 BP = Blueprint('main', __name__)
 
@@ -39,6 +39,17 @@ def show_batch_jobs():
     else:
         username = None
     return render_template('main/jobs.html', username=username)
+
+@BP.route("/jobs/<jobid>/log")
+@login_required
+def show_job_log(jobid):
+    job = BatchJob.query.filter_by(id=jobid).first()
+    if current_user.username == job.user and job.viewed == False:
+        job.viewed = True
+        db.session.add(job)
+        db.session.commit()
+    return render_template('main/job_log.html', jobId=job.id, jobName=job.name, submittedOn=job.submitted_on,
+                           logStreamName=job.log_stream_name, status=job.status)
 
 @BP.route("/illumina_qc")
 @login_required
